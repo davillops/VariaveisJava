2253ee84fe8896258f269ee1b59f0c49eec53bd5
davillops <davi_demedeiros@hotmail.com>
2022-05-06 18:44:07 -0300

    VariaveisJava

---
+++ src/App.java
@@ -0,0 +1,41 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+    // Variaveis do tipo inteiro
+
+    // tipo  nome
+        byte numeroPequeno = 125;
+        short numeroShort = 32767;
+        int numeroMaior = 2147483647;
+        long numeroEnorme = 922337203685477000l;
+
+    // Variaveis do tipo ponto flutuante
+
+        float peso = 78.7f;
+        double pi = 3.14159265;
+
+    // Variaveis do tipo caractere
+
+        char letra = 'C';
+
+    // Variaveis do tipo booleano **TRUE ou FALSE**
+
+        boolean estouComFome = true;
+        boolean estouComSono = false;
+
+    // Variaveis do tipo String
+
+        String nome = "Lucas";
+        String nomeCompleto = "Fabio Silva";
+
+
+        System.out.println(numeroPequeno);
+        System.out.println(numeroShort);
+        System.out.println(numeroMaior);
+        System.out.println(numeroEnorme);
+
+        System.out.println(nome);
+        System.out.println(nomeCompleto);
+
+    }
+}
